f1f4535064f112795cfb2e8b35fe705a721c5286
Anabella Spinelli <anabellaspinelli@gmail.com>
2018-04-02 10:32:55 +0200

    Video 6 completed: added client ID and secret.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,7 +3,10 @@ const GoogleStrategy = require('passport-google-oauth20');
 
 passport.use(
   new GoogleStrategy({
-    //options
+    //options for the google strategy
+    clientID:
+      '468235828330-60laqlu9nv57ki2o67qv8fvefbjnkvgq.apps.googleusercontent.com',
+    clientSecret: '3LHTkx5Se5pvMOTSSgPHnzF6'
   }),
   () => {
     // passport callback function
