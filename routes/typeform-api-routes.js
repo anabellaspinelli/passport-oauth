4c530b765c2558a7c4c10fa9d4ae9ce436810695
Anabella Spinelli <anabella.spinelli@typeform.com>
2018-04-06 17:34:13 +0200

    Added routes for GET webhooks and responses (from a form).

--- routes/typeform-api-routes.js
+++ routes/typeform-api-routes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 const request = require('superagent');
-const scopes = require('../config/scopes');
 
 const TF_API_BASE = 'https://api.typeform.com';
 
-scopes.forEach(scope => {
+['forms', 'images', 'themes'].forEach(scope => {
   router.get(`/${scope}`, (req, res) => {
     request
       .get(`${TF_API_BASE}/${scope}`)
@@ -18,6 +17,46 @@ scopes.forEach(scope => {
       // eslint-disable-next-line no-console
       .catch(e => console.error(e));
   });
+});
+
+router.get('/responses', (req, res) => {
+  request
+    .get(`${TF_API_BASE}/forms`)
+    .set({ Authorization: `Bearer ${req.user.access_token}` })
+    .then(formsResponse => formsResponse.body.items[0].id)
+    .then(formId => {
+      return request
+        .get(`${TF_API_BASE}/forms/${formId}/responses`)
+        .set({ Authorization: `Bearer ${req.user.access_token}` });
+    })
+    .then(responsesResponse => {
+      res.send({
+        typeformResponseBody: responsesResponse.body,
+        typeformResponseStatus: responsesResponse.status
+      });
+    })
+    // eslint-disable-next-line no-console
+    .catch(e => console.error(e));
+});
+
+router.get('/webhooks', (req, res) => {
+  request
+    .get(`${TF_API_BASE}/forms`)
+    .set({ Authorization: `Bearer ${req.user.access_token}` })
+    .then(formsResponse => formsResponse.body.items[0].id)
+    .then(formId => {
+      return request
+        .get(`${TF_API_BASE}/forms/${formId}/webhooks/tag`)
+        .set({ Authorization: `Bearer ${req.user.access_token}` });
+    })
+    .then(webhooksResponse => {
+      res.send({
+        typeformResponseBody: webhooksResponse.body,
+        typeformResponseStatus: webhooksResponse.status
+      });
+    })
+    // eslint-disable-next-line no-console
+    .catch(e => console.error(e));
 });
 
 module.exports = router;
