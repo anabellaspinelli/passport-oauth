ce8e7241c4acc3a9e0c85d7a979dd3add49dc347
Anabella Spinelli <anabella.spinelli@typeform.com>
2018-04-10 09:46:46 +0200

    Changed scopes config format to obj.

--- config/scopes.js
+++ config/scopes.js
@@ -1,3 +1,9 @@
-const scopes = ['forms', 'images', 'themes', 'responses', 'webhooks'];
+const scopes = {
+  forms: ['read', 'write'],
+  images: ['read', 'write'],
+  themes: ['read', 'write'],
+  responses: ['read'],
+  webhooks: ['read', 'write']
+};
 
 module.exports = scopes;
