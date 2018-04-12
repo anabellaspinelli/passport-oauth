9314610bb510689ba0e052315f9f7d42fe6bf371
Anabella Spinelli <anabella.spinelli@typeform.com>
2018-04-12 18:51:38 +0200

    Added routes to post forms, themes and images.

--- routes/typeform-api-routes.js
+++ routes/typeform-api-routes.js
@@ -1,28 +1,39 @@
 const router = require('express').Router();
 const request = require('superagent');
+const fs = require('fs');
+const path = require('path');
 
 const TF_API_BASE = 'https://api.typeform.com';
 
-['forms', 'images', 'themes'].forEach(scope => {
-  router.get(`/${scope}`, (req, res) => {
-    request
-      .get(`${TF_API_BASE}/${scope}`)
-      .set({ Authorization: `Bearer ${req.user.access_token}` })
-      .then(response => {
-        res.send({
-          typeformResponseBody: response.body,
-          typeformResponseStatus: res.status
-        });
-      })
-      // eslint-disable-next-line no-console
-      .catch(e => console.error(e));
-  });
+router.get('/forms', (req, res) => {
+  getFromTypeform('forms', req).then(response =>
+    res.send({
+      typeformResponseBody: response.body,
+      typeformResponseStatus: res.status
+    })
+  );
+});
+
+router.get('/images', (req, res) => {
+  getFromTypeform('images', req).then(response =>
+    res.send({
+      typeformResponseBody: response.body,
+      typeformResponseStatus: res.status
+    })
+  );
+});
+
+router.get('/themes', (req, res) => {
+  getFromTypeform('themes', req).then(response =>
+    res.send({
+      typeformResponseBody: response.body,
+      typeformResponseStatus: res.status
+    })
+  );
 });
 
 router.get('/responses', (req, res) => {
-  request
-    .get(`${TF_API_BASE}/forms`)
-    .set({ Authorization: `Bearer ${req.user.access_token}` })
+  getFromTypeform('forms', req)
     .then(formsResponse => formsResponse.body.items[0].id)
     .then(formId => {
       return request
@@ -40,9 +51,7 @@ router.get('/responses', (req, res) => {
 });
 
 router.get('/webhooks', (req, res) => {
-  request
-    .get(`${TF_API_BASE}/forms`)
-    .set({ Authorization: `Bearer ${req.user.access_token}` })
+  getFromTypeform('forms', req)
     .then(formsResponse => formsResponse.body.items[0].id)
     .then(formId => {
       return request
@@ -59,4 +68,65 @@ router.get('/webhooks', (req, res) => {
     .catch(e => console.error(e));
 });
 
+router.post('/forms', (req, res) => {
+  postToTypeform('forms', req, { title: 'oauth test' }).then(
+    postFormResponse => {
+      res.send({
+        typeformResponseBody: postFormResponse.body,
+        typeformResponseStatus: postFormResponse.status
+      });
+    }
+  );
+});
+
+router.post('/themes', (req, res) => {
+  postToTypeform('themes', req, { name: 'oauth test' }).then(
+    postFormResponse => {
+      res.send({
+        typeformResponseBody: postFormResponse.body,
+        typeformResponseStatus: postFormResponse.status
+      });
+    }
+  );
+});
+
+router.post('/images', (req, res) => {
+  fs.readFile(
+    path.resolve(__dirname, '../public/img/beach.jpeg'),
+    'base64',
+    (err, data) => {
+      if (err) throw err;
+      console.log('readFile');
+      request
+        .post(`${TF_API_BASE}/images`)
+        .set({ Authorization: `Bearer ${req.user.access_token}` })
+        .send({
+          image: data,
+          file_name: 'ana.png'
+        })
+        .then(postImageResponse => {
+          console.log('tf response', postImageResponse.body);
+          res.send({
+            typeformResponseBody: postImageResponse.body,
+            typeformResponseStatus: postImageResponse.status
+          });
+        })
+        .catch(err => console.log(err));
+    }
+  );
+});
+
 module.exports = router;
+
+function getFromTypeform(collection, req) {
+  return request
+    .get(`${TF_API_BASE}/${collection}`)
+    .set({ Authorization: `Bearer ${req.user.access_token}` });
+}
+
+function postToTypeform(collection, req, body) {
+  return request
+    .post(`${TF_API_BASE / collection}`)
+    .set({ Authorization: `Bearer ${req.user.access_token}` })
+    .send(body);
+}
